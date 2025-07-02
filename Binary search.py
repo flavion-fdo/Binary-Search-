18382913bb297aaa316c3490ac6c633e561887c7
flavion-fdo <flavi23fdo@gmail.com>
2025-07-02 06:32:19 -0700

    Add files via upload
    Can edit the array to your will

---
+++ Binary search.py
@@ -0,0 +1,29 @@
+Ar=[1,2,5,8,9,10,11,15,16,19]
+print (Ar)
+def BS(find):
+    L=0
+    H=len(Ar)-1
+    found= False
+    not_found= False
+    while found==False and not_found==False:
+        #calculate midpoint 
+        M=(L+H)//2
+        #check if item is found
+        if Ar[M]==find:
+            found=True
+        elif L>=H:
+            not_found=True
+        elif Ar[M]<find:
+            L=M+1
+        else:
+            H=M-1
+    if found==True:
+        x=("Item Found")
+    else:
+        x=("Item Not Found")
+    return x
+
+#main
+n=int(input("Enter number to search "))
+Result=BS(n)
+print(Result)
